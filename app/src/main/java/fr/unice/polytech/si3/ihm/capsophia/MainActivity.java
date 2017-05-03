62fefb7d3a8ee50095d1c5c5f22ae2699c29232e
Julien Lemaire <julien.lemaire@etu.unice.fr>
2017-05-03 15:51:08 +0200

    Google Maps in a nicer activity

--- app/src/main/java/fr/unice/polytech/si3/ihm/capsophia/MainActivity.java
+++ app/src/main/java/fr/unice/polytech/si3/ihm/capsophia/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.si3.ihm.capsophia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -103,7 +104,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
